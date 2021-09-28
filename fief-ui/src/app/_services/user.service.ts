df27f0e9300963a2fcd8007a911523702c16c62c
Guillaume Fouillet <guillaume.fouillet+github@gmail.com>
2021-09-28 20:49:39 +0200

    feat(ui): Register and login page

--- fief-ui/src/app/_services/user.service.ts
+++ fief-ui/src/app/_services/user.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
 import { User } from '@app/_models';
+import { first, map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
@@ -11,4 +12,10 @@ export class UserService {
     getAll() {
         return this.http.get<User[]>(`${environment.apiUrl}/users`);
     }
+
+
+    register(username: string, email: string, password: string) {
+      return this.http.post<any>(`${environment.apiUrl}/auth/signup`, { username, email, password });
+    }
+
 }
--- fief-ui/src/app/register/register.component.ts
+++ fief-ui/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService, UserService } from '@app/_services';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.less']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  userCreated = false;
+  submitted = false;
+  error = '';
 
-  constructor() { }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    ) {
+      // redirect to home if already logged in
+      if (this.authenticationService.currentUserValue) {
+          this.router.navigate(['/']);
+      }
+  }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+        email: ['', Validators.required],
+    });
+  }
+
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+
+    onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.registerForm.invalid) {
+          return;
+      }
+
+      this.loading = true;
+      this.userService.register(this.f.username.value, this.f.email.value, this.f.password.value)
+          .pipe(first())
+          .subscribe({
+              next: () => {
+                  this.userCreated = true;
+              },
+              error: error => {
+                  this.error = error;
+                  this.loading = false;
+              }
+          });
   }
 
 }
