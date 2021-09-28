343be6814cab2b77e52e29ff5e0a850feaaa52af
Guillaume Fouillet <guillaume.fouillet+github@gmail.com>
2021-09-28 21:49:26 +0200

    chore(ui): Remove Vue backup (I am happy with my angular stuff)

--- fief-ui.back/src/services/auth.constants.ts
+++
@@ -1,4 +0,0 @@
-
-export const LOCAL_STORAGE_AUTH_TOKEN_KEY = 'authToken'
-export const LOCAL_STORAGE_USER_INFO_KEY = 'userInfos'
-export const API_URL = 'http://localhost:3000/api/v1/'
--- fief-ui.back/src/types/users.ts
+++
@@ -1,4 +0,0 @@
-
-export type UserInfos = {
-  login: string,
-}
